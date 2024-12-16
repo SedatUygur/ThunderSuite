f5d958fe361997b891a0620f7120eed5108a6eed
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-16 22:18:32 +0300

    ➕ add state management system

--- framework/index.js
+++ framework/index.js
@@ -8,3 +8,25 @@ export const initial = (selector, component) => {
     const app = document.querySelector(selector);
     patch(app, component.template);
 };
+
+let state = {};
+
+export const createComponent = ({
+  template,
+  methods = {},
+  initialState = {}
+}) => {
+  state = initialState;
+  const mappedMethods = Object.keys(methods).reduce(
+    (acc, key) => ({
+      ...acc,
+      [key]: (...args) => {
+        state = methods[key](state, ...args);
+        console.log(state);
+        return state;
+      }
+    }),
+    {}
+  );
+  return props => template({ ...props, ...state, methods: mappedMethods });
+};
--- src/user.js
+++ src/user.js
@@ -1,7 +1,16 @@
+import { createComponent } from "../framework";
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
 const firstName = "Sedat";
 const lastName = "Uygur";
 
-export const User = ({ firstName, lastName }) => div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}`;
+const methods = { changeName: (state, firstName) => ({ ...state, firstName }) };
+const initialState = { firstName: firstName, lastName: lastName };
+
+const template = ({ firstName, lastName, methods }) =>
+  div`${onClick(() =>
+    methods.changeName("Sedat Can")
+  )} Hello ${firstName} ${lastName}`;
+
+export const User = createComponent({ template, methods, initialState });
