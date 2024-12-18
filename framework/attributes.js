e653e878b42a74c24f9668596c762556d6c4fcb9
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-18 22:20:43 +0300

    📝 implement attribute setter methods

--- framework/attributes.js
+++ framework/attributes.js
@@ -1,23 +1,52 @@
-export function setAttributes(el, attrs) {
+export const setAttributes = (el, attrs) => {
     const { class: className, style, ...otherAttrs } = attrs
 
     if (className) {
-        //setClass(el, className)
+        setClass(el, className)
     }
 
     if (style) {
         Object.entries(style).forEach(([prop, value]) => {
-            //setStyle(el, prop, value)
+            setStyle(el, prop, value)
         })
     }
 
     for (const [name, value] of Object.entries(otherAttrs)) {
-        //setAttribute(el, name, value)
+        setAttribute(el, name, value)
     }
 }
 
-// TODO: implement setClass
+const setClass = (el, className) => {
+    el.className = ''
+  
+    if (typeof className === 'string') {
+      el.className = className
+    }
+  
+    if (Array.isArray(className)) {
+      el.classList.add(...className)
+    }
+}
 
-// TODO: implement setStyle
+export const setStyle = (el, name, value) => {
+    el.style[name] = value
+}
+  
+export const removeStyle = (el, name) => {
+    el.style[name] = null
+}
 
-// TODO: implement setAttribute
+export const setAttribute = (el, name, value) => {
+    if (value == null) {
+        removeAttribute(el, name)
+    } else if (name.startsWith('data-')) {
+        el.setAttribute(name, value)
+    } else {
+        el[name] = value
+    }
+}
+
+export const removeAttribute = (el, name) => {
+    el[name] = null
+    el.removeAttribute(name)
+}
